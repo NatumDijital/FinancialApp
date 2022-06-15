d1ace6b851ef8f7598d2a7755b6e7c283f2ccd25
BernaColakel <102745096+BernaColakel@users.noreply.github.com>
2022-06-15 17:15:24 +0300

    first onboarding page is added
    working on second page..

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -1,4 +1,4 @@
-import { Animated, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native';
+import { Animated, ImageBackground, SafeAreaView, StyleSheet, TouchableOpacity, Dimensions, Pressable } from 'react-native';
 import PagerView from 'react-native-pager-view';
 
 import { Text, View } from '../components/Themed';
@@ -8,6 +8,7 @@ import { RootStackScreenProps } from '../types';
 const AnimatedPagerView = Animated.createAnimatedComponent(PagerView);
 
 export default function OnboardingScreen({ navigation }: RootStackScreenProps<'Onboarding'>) {
+
   return (
     <SafeAreaView style={styles.container}>
       <PagerView
@@ -15,15 +16,36 @@ export default function OnboardingScreen({ navigation }: RootStackScreenProps<'O
         initialPage={0}
         showPageIndicator={true}
       >
-        <View style={styles.page} key="1">
-          <Text>First page</Text>
-          <Text>Swipe ➡️</Text>
+        <View style={styles.page} key="1" >
+          <ImageBackground source={require('../assets/images/onboarding-background-image.png')} style={styles.backgroundImage}>
+            <View style={styles.firstPageContainer}>
+              <Text style={styles.boldText}>Everthing you need to invest.</Text>
+              <Text style={styles.text}>A finance news experience that is noiseless, focused and far from the chaos of social media and classical news.</Text>
+            </View>
+          </ImageBackground>
         </View>
+
+
         <View style={styles.page} key="2">
-          <Text>Second page</Text>
+          <ImageBackground source={require('../assets/images/onboarding-background-image.png')} style={styles.backgroundImage}>
+            <Text style={styles.skipText}>Skip</Text>
+            <View style={styles.secondPageContainer}>
+              <Text style={styles.boldText}>Which stocks are you interested in?</Text>
+              <Text style={styles.text}>Select all that apply. If you cant find your stock please search at below.</Text>
+            </View>
+            <View style={styles.secondPageContainer}>
+              <TouchableOpacity style={styles.buttons}>
+                <Text >AAPL</Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
         </View>
+
+
         <View style={styles.page} key="3">
-          <Text>Third page</Text>
+          <ImageBackground source={require('../assets/images/onboarding-background-image.png')} style={styles.backgroundImage}>
+            <Text>Third page</Text>
+          </ImageBackground>
         </View>
       </PagerView>
     </SafeAreaView>
@@ -44,4 +66,53 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'center',
+    backgroundColor: Colors.background,
+    width: Dimensions.get("window").width, //for full screen
+    height: Dimensions.get("window").height //for full screen
+  },
+  boldText: {
+    fontWeight: '700',
+    fontSize: 34,
+    color: Colors.Text.white,
+  },
+  text: {
+    fontWeight: '400',
+    fontSize: 16,
+    color: Colors.Text.white
+  },
+  firstPageContainer: {
+    backgroundColor: 'transparent',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginTop: 400,
+    marginHorizontal: 43,
+    alignItems: 'flex-end',
+    flex: 1,
+    marginBottom: 170,
+    alignContent: 'stretch'
+  },
+  skipText: {
+    fontWeight: '600',
+    fontSize: 18,
+    color: Colors.Text.white,
+    textAlign: 'right',
+    margin: 12
+  },
+  secondPageContainer: {
+    backgroundColor: 'transparent',
+    alignContent: 'center',
+    margin: 27.5,
+    flex: 1
+  },
+  buttons: {
+    borderRadius: 20,
+    height: 45,
+    width: 99,
+    backgroundColor: Colors.Text.white,
+    textAlign: 'center',
+    padding: 12
+  }
 });
