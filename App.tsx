4d35136c4f1b5ec0a345c4075fc80d4635d72439
BernaColakel <102745096+BernaColakel@users.noreply.github.com>
2022-06-28 19:36:19 +0300

    Problem is occurred

--- App.tsx
+++ App.tsx
@@ -4,11 +4,17 @@ import { initialWindowMetrics, SafeAreaProvider } from 'react-native-safe-area-c
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import * as RNLocalize from "react-native-localize";
+import { I18nManager } from 'react-native';
+import i18n from 'i18n-js';
+const memoize = require('memoize');
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  
+  setI18nConfig();
+  
   if (!isLoadingComplete) {
     return null;
   } else {
@@ -24,3 +30,36 @@ export default function App() {
     );
   }
 }
+
+const translationGetters = {
+  en: () => require("./i18n/en.json"),
+  tr: () => require("./i18n/tr.json"),
+};
+
+const translate = memoize(
+  (key, config) => i18n.t(key, config),
+  (key, config) => (config ? key + JSON.stringify(config) : key),
+);
+
+
+const setI18nConfig = () => {
+
+  // fallback if no available language fits
+  const fallback = { languageTag: "en", isRTL: false };
+
+  const { languageTag, isRTL } =
+    RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
+    fallback;
+
+  // clear translation cache
+  translate.cache.clear();
+  // update layout direction
+  I18nManager.forceRTL(isRTL);
+
+  // set i18n-js config
+  i18n.translations = {
+    [languageTag]: translationGetters[languageTag](),
+  };
+
+  i18n.locale = languageTag;
+};
---
+++ localization/languages.tsx
@@ -0,0 +1,9 @@
+import tr from "../i18n/tr.json";
+import en from "../i18n/en.json";
+
+const translations = {
+    tr: tr,
+    en: en,
+}
+
+export default translations;
